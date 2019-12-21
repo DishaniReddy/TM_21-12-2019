a89bd1cd1e67d32d66549670a0b0d01b0d1ea310
Dishu <Dishu@192.168.1.12>
2019-12-21 09:51:04 +0530

    First Commit [21/2/2019]

---
+++ src/test/java/com/qa/tests/HRM_Login.java
@@ -0,0 +1,19 @@
+package com.qa.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HRM_Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dishu\\Work-Selenium\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);			
+		driver.get("http://testingmasters.com/hrm/");
+
+	}
+
+}
